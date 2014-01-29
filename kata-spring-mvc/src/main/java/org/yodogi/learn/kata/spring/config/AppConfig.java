2f77f946316dcb3f4b311900f8e52d62aa7048ad
Yovany Dominico <yodogi@gmail.com>
2014-01-29 15:43:42 +0100

    Remove external bussines logic

--- kata-spring-mvc/src/main/java/org/yodogi/learn/kata/spring/config/AppConfig.java
+++ kata-spring-mvc/src/main/java/org/yodogi/learn/kata/spring/config/AppConfig.java
@@ -8,11 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.web.client.RestTemplate;
-import org.yodogi.learn.kata.spring.security.RestAuthenticationProvider;
 
 @Configuration
 @PropertySource("classpath:dynamic.properties")
@@ -26,25 +25,18 @@ public class AppConfig {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
-	public @Bean
-	AuthenticationProvider restAuthenticationProvider() {
-		RestAuthenticationProvider restAuth = new RestAuthenticationProvider();
-		// restAuth.setRestServiceUrl(env.getProperty("rest.service.url"));
-		// restAuth.setRestServiceAttrTicket(env.getProperty("{rest.service.attribute.ticket"));
-		return restAuth;
-	}
-
 	@SuppressWarnings("deprecation")
 	public @Bean
 	RestTemplate restTemplate() {
 		return new RestTemplate(new HttpComponentsClientHttpRequestFactory(new DefaultHttpClient(new PoolingClientConnectionManager())));
 	}
 
-	/*
-	 * <bean id="httpClient" class="org.apache.http.impl.client.DefaultHttpClient"> <constructor-arg> <bean
-	 * class="org.apache.http.impl.conn.PoolingClientConnectionManager"/> </constructor-arg> </bean>
-	 * 
-	 * <bean id="restTemplate" class="org.springframework.web.client.RestTemplate"> <constructor-arg> <bean
-	 * class="org.springframework.http.client.HttpComponentsClientHttpRequestFactory"> <constructor-arg ref="httpClient"/> </bean> </constructor-arg> </bean>
-	 */
+	public @Bean
+	ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages/messages");
+		messageSource.setCacheSeconds(0);
+		return messageSource;
+	}
+
 }
--- kata-spring-mvc/src/main/java/org/yodogi/learn/kata/spring/web/SpringSecurityController.java
+++ kata-spring-mvc/src/main/java/org/yodogi/learn/kata/spring/web/SpringSecurityController.java
@@ -58,8 +58,4 @@ public class SpringSecurityController {
 
 	}
 
-	private class Ticket {
-		String ticket;
-	}
-
 }
